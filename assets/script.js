e81d82da606bc89b71b1df93b24caef9c777bd75
oliverLo78 <oliverberto@gmail.com>
2022-10-16 16:00:42 -0400

    added placeholder for picture

--- assets/script.js
+++ assets/script.js
@@ -36,23 +36,79 @@
 
 // ****************************** Handlers ***************************************************** //
 // Select increment and decrement button elements
-var count = 0;
-var btnSearchEl = document.querySelector("#increment");
-var countEl = document.querySelector("#count");
+// var count = 0;
+// var btnSearchEl = document.querySelector("#increment");
+// var countEl = document.querySelector("#count");
 // var hideCardsEl = document.querySelector("#hideCards");
 
 // Updates count on page
-function setCounterText() {
-    countEl.textContent = count;
-}
+// function setCounterText() {
+//     countEl.textContent = count;
+// }
 
-function increment(event) {
-    console.log(event);
+// function increment(event) {
+    // console.log(event);
     //
-}
-
+//}
 
 // Attach event listener to increment button element
-btnSearchEl.addEventListener("click", increment);
+// btnSearchEl.addEventListener("click", increment);
+
 // Attach event listener to decrement button element
-hideCardsEl.addEventListener("click", hideCardsEl);
+// hideCardsEl.addEventListener("click", hideCardsEl);
+
+
+/******************************************************************************************************* */
+var drink;
+var input;
+var requestDrink;
+var apiRootCocktailURL = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?';
+var requestVideo;
+var apiRootYouTubeURL = 'https://www.googleapis.com/youtube/v3/search?';
+
+$('input').keyup(function(e){
+    e.preventDefault();
+    input = $('input#userInput').val();
+    console.log(input);
+});
+
+$('#search').click(function drinkParams(){
+    var paramsString = 's=margarita';
+    var searchParams = new URLSearchParams(paramsString);
+    searchParams.set('s', input);
+    var x = searchParams.toString();
+    requestDrink = apiRootCocktailURL+x;
+    console.log(requestDrink);
+    getDrink(requestDrink);
+});
+
+$('#search').click(function videoParams(){
+    var paramsString = 'q={search term}&type=video&part=snippet&key=AIzaSyC5toGw1SSB32wE6uogT2Hk25_CWavryVo';
+    var searchParams = new URLSearchParams(paramsString);
+    searchParams.set('q', input);
+    var x = searchParams.toString();
+    requestVideo = apiRootYouTubeURL+x;
+    console.log(requestVideo);
+    getVideo(requestVideo);
+});
+
+function getDrink(requestDrink){
+    fetch(requestDrink)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(data){
+        console.log(data);
+        });
+}
+
+function getVideo(requestVideo){
+    fetch(requestVideo)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(data){
+        console.log(data);
+        });
+}
+
