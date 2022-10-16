7e640cd5f431f93e08e12fea80341178203661ec
Volantis <putzstuck@gmail.com>
2022-10-16 13:04:38 -0400

    updated js file with dynamically created elements

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,24 @@ var input;
 var requestDrink;
 var apiRootCocktailURL = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?';
 var requestVideo;
-var apiRootYouTubeURL = 'https://www.googleapis.com/youtube/v3/search?';
+var apiRootYouTubeURL = 'https://www.googleapis.com/youtube/v3/search?maxResults=3&safeSearch=moderate&videoEmbeddable=true&type=video&part=snippet&key=AIzaSyC5toGw1SSB32wE6uogT2Hk25_CWavryVo&';
+var youTubeVid1;
+var youTubeVid2;
+var youTubeVid3;
+var youTubeRoot = 'https://www.youtube.com/embed/';
+var searchResultsVid1;
+var searchResultsVid2;
+var searchResultsVid3;
+
+$(document).ready(function(){
 
 $('input').keyup(function(e){
     e.preventDefault();
     input = $('input#userInput').val();
-    console.log(input);
 });
 
 $('#search').click(function drinkParams(){
-    var paramsString = 's=margarita';
+    var paramsString = 's={drink name}';
     var searchParams = new URLSearchParams(paramsString);
     searchParams.set('s', input);
     var x = searchParams.toString();
@@ -22,9 +30,9 @@ $('#search').click(function drinkParams(){
 });
 
 $('#search').click(function videoParams(){
-    var paramsString = 'q={search term}&type=video&part=snippet&key=AIzaSyC5toGw1SSB32wE6uogT2Hk25_CWavryVo';
+    var paramsString = 'q={search term}';
     var searchParams = new URLSearchParams(paramsString);
-    searchParams.set('q', input);
+    searchParams.set('q', 'recipe'+input);
     var x = searchParams.toString();
     requestVideo = apiRootYouTubeURL+x;
     console.log(requestVideo);
@@ -34,19 +42,81 @@ $('#search').click(function videoParams(){
 function getDrink(requestDrink){
     fetch(requestDrink)
         .then(function(response){
-        return response.json();
+        return response.json()
         })
         .then(function(data){
-        console.log(data);
+            // for (var i = 0; i < data.drinks.length; i++) {
+            // console.log(data.drinks);
+            var drinkName = document.createElement('h2');
+            drinkName.textContent = data.drinks[0].strDrink;
+            $('div#drinkName').append(drinkName);
+            var drinkIng1 = document.createElement('li');
+            drinkIng1.textContent = data.drinks[0].strIngredient1;
+            var drinkIng2 = document.createElement('li');
+            drinkIng2.textContent = data.drinks[0].strIngredient2;
+            var drinkIng3 = document.createElement('li');
+            drinkIng3.textContent = data.drinks[0].strIngredient3;
+            var drinkIng4 = document.createElement('li');
+            drinkIng4.textContent = data.drinks[0].strIngredient4;
+            var drinkIng5 = document.createElement('li');
+            drinkIng5.textContent = data.drinks[0].strIngredient5;
+            var a = document.createElement('ul');
+            $(a).append(drinkIng1);
+            $(a).append(drinkIng2);
+            $(a).append(drinkIng3);
+            $(a).append(drinkIng4);
+            $(a).append(drinkIng5);
+            $('div#drinkIngr').append(a);
+            var drinkInst = document.createElement('p');
+            drinkInst.textContent = data.drinks[0].strInstructions;
+            $('div#drinkInst').append(drinkInst);
+            // }
         });
 }
 
 function getVideo(requestVideo){
     fetch(requestVideo)
         .then(function(response){
-        return response.json();
+        return response.json()
         })
         .then(function(data){
-        console.log(data);
+            for (var i = 0; i < data.items.length; i++) {
+            // console.log(data.items);
+            searchResultsVid1 = data.items[0].id.videoId;
+            searchResultsVid2 = data.items[1].id.videoId;
+            searchResultsVid3 = data.items[2].id.videoId;
+            }
+        youTubeVid1 = youTubeRoot + searchResultsVid1;
+        youTubeVid2 = youTubeRoot + searchResultsVid2;
+        youTubeVid3 = youTubeRoot + searchResultsVid3;
+        var a1 = document.createElement('iframe');
+        a1.setAttribute('class','has-ratio');
+        a1.setAttribute('width','640');
+        a1.setAttribute('height','360');
+        a1.setAttribute('src',youTubeVid1);
+        var b1 = document.createElement('figure');
+        b1.setAttribute('class','image is-16by9');
+        $(b1).append(a1);
+        $('div#youTubeVid1').append(b1);
+        var a2 = document.createElement('iframe');
+        a2.setAttribute('class','has-ratio');
+        a2.setAttribute('width','320');
+        a2.setAttribute('height','180');
+        a2.setAttribute('src',youTubeVid2);
+        var b2 = document.createElement('figure');
+        b2.setAttribute('class','image is-16by9');
+        $(b2).append(a2);
+        $('div#youTubeVid2').append(b2);  
+        var a3 = document.createElement('iframe');
+        a3.setAttribute('class','has-ratio');
+        a3.setAttribute('width','320');
+        a3.setAttribute('height','180');
+        a3.setAttribute('src',youTubeVid3);
+        var b3 = document.createElement('figure');
+        b3.setAttribute('class','image is-16by9');
+        $(b3).append(a3);      
+        $('div#youTubeVid3').append(b3);        
         });
 }
+
+});
